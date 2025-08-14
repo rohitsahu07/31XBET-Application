b911364643daec33d53a11a0ffc49960eb33309e
rohitsahu07 <rohitsahu704980@gmail.com>
2025-08-14 18:36:43 +0530

    delete all code

--- backend/bets/urls.py
+++
@@ -1,8 +0,0 @@
-from rest_framework.routers import DefaultRouter
-from .views import GameViewSet, BetViewSet
-
-router = DefaultRouter()
-router.register(r'games', GameViewSet, basename='game')
-router.register(r'', BetViewSet, basename='bet')
-
-urlpatterns = router.urls
--- backend/ledger/urls.py
+++
@@ -1,7 +0,0 @@
-from rest_framework.routers import DefaultRouter
-from .views import LedgerViewSet
-
-router = DefaultRouter()
-router.register(r'', LedgerViewSet, basename='ledger')
-
-urlpatterns = router.urls
